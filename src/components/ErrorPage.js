207a09b65a6abe5a6c7ceeb19e0f50585ea7c499
vishwas-yadav-cell <vishwasyadav164@gmail.com>
2020-11-29 15:18:11 +0530

    'inital'

--- src/components/ErrorPage.js
+++ src/components/ErrorPage.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 
 const ErrorPage = () => {
+    const before = useHistory();
+    console.log(before);
+
     return (
         <div>
             <h1>this is 404 Page...</h1>
+            <h5 onClick={()=>before.goBack()}>CLICK ME TO GO BACK</h5>
         </div>
     );
 }
